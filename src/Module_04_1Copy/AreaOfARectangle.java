abaf25d193f7d22ab43cbc25fbea489579df0416
Oleg Kabysh <okabysh@gmail.com>
2016-09-11 22:48:06 +0300

    mod.7 from 27.07.2016 changed class ComputerPlayer  "private void parseGameFieldToIntField()" to "public void parseGameFieldToIntField()"; add new class ComputerPlayerTest.

---
+++ src/Module_04_1Copy/AreaOfARectangle.java
@@ -0,0 +1,17 @@
+package src.Module_04_1;
+
+public class AreaOfARectangle {
+    private int sideA;
+    private int sideB;
+    private int area;
+    AreaOfARectangle(int sideA, int sideB) {
+        this.sideA = sideA;
+        this.sideB = sideB;
+    }
+
+    public int getArea() {
+        area = sideA * sideB;
+        return area;
+
+    }
+}
---
+++ src/Module_04_1Copy/AreaOfATriangle.java
@@ -0,0 +1,16 @@
+package src.Module_04_1;
+
+public class AreaOfATriangle {
+    private int sideA;
+    private int height;
+    private float area;
+    AreaOfATriangle(int sideA, int height) {
+        this.sideA = sideA;
+        this.height = height;
+    }
+
+    public float getArea() {
+        area = 0.5f * sideA * height;
+        return area;
+    }
+}
