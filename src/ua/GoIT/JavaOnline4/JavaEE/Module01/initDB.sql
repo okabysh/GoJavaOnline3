6fbb4057be6b4f8cb7d240c22309f1a8dfa5ae16
Oleg Kabysh <okabysh@gmail.com>
2016-11-28 01:05:25 +0200

    Java4_EE Module_01 mod.1 from 28.11.2016

---
+++ src/ua/GoIT/JavaOnline4/JavaEE/Module01/initDB.sql
@@ -0,0 +1,70 @@
+﻿CREATE TABLE COMPANIES(
+_ID integer PRIMARY KEY NOT NULL,
+_Descryption varchar(200) NOT NULL,
+Identification_Code varchar(14),
+Telephones varchar(70),
+Address varchar(120),
+Comment text
+);
+
+CREATE TABLE PROJECTS(
+_ID integer PRIMARY KEY NOT NULL,
+_Descryption varchar(200) NOT NULL,
+Comment text
+);
+
+CREATE TABLE CUSTOMERS(
+_ID integer PRIMARY KEY NOT NULL,
+_Descryption varchar(200) NOT NULL,
+Identification_Code varchar(14),
+Telephones varchar(70),
+Address varchar(120),
+Comment text
+);
+
+CREATE TABLE SKILLS(
+_ID integer PRIMARY KEY NOT NULL,
+_Descryption varchar(200) NOT NULL,
+Comment text
+);
+
+
+CREATE TABLE CUSTOMER_PROJECTS(
+_ID integer PRIMARY KEY NOT NULL,
+Customer_Ref integer references CUSTOMERS(_ID),
+Project_Ref integer references PROJECTS(_ID), 
+Comment text
+);
+
+
+
+CREATE TABLE DEVELOPERS(
+_ID integer PRIMARY KEY NOT NULL,
+_Descryption varchar(200) NOT NULL,
+First_Name varchar(70),
+Last_Name varchar(70),
+Patronymic varchar(70),
+Identification_Code varchar(10),
+DateOfBirth date,
+Telephones varchar(70),
+Address varchar(120),
+Company_Ref integer references COMPANIES(_ID),
+Comment text
+);
+
+CREATE TABLE DEVELOPER_SKILLS(
+_ID integer PRIMARY KEY NOT NULL,
+Developer_Ref integer references DEVELOPERS(_ID),
+Skill_Ref integer references SKILLS(_ID), 
+Comment text
+);
+
+CREATE TABLE DEVELOPER_CUSTOMERS_PROJECTS(
+_ID integer PRIMARY KEY NOT NULL,
+Date_Start date,
+Date_Deadline date,
+Developer_Ref integer references DEVELOPERS(_ID),
+Customer_Project_Ref integer references CUSTOMER_PROJECTS(_ID), 
+Comment text
+);
+ 
