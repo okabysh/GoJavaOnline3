edd60faa7080f704071c708d2e33c72c426767c0
Oleg KABYSH <okabysh@gmail.com>
2016-11-29 14:02:34 +0200

    mod.2 from 29.11.2016

--- src/ua/GoIT/JavaOnline4/JavaEE/Module01/Hometask_1_2/1.sql
+++ src/ua/GoIT/JavaOnline4/JavaEE/Module01/Hometask_1_2/1.sql
@@ -1,1 +1,6 @@
 ﻿ALTER TABLE developers ADD Salary real
+
+UPDATE developers SET salary = 2000 WHERE _ID=1;
+UPDATE developers SET salary = 2100 WHERE _ID=2;
+UPDATE developers SET salary = 2200 WHERE _ID=3;
+UPDATE developers SET salary = 2300 WHERE _ID=4;
