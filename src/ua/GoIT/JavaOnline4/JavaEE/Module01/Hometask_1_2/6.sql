f9a12e2835af884b3cb463935e0bf41f47f9561d
Oleg KABYSH <okabysh@gmail.com>
2016-11-29 17:36:27 +0200

    mod.2 from 29.11.2016

--- src/ua/GoIT/JavaOnline4/JavaEE/Module01/Hometask_1_2/6.sql
+++ src/ua/GoIT/JavaOnline4/JavaEE/Module01/Hometask_1_2/6.sql
@@ -1,1 +1,16 @@
-﻿
+SELECT
+	SUM(developer_customers_projects.term * developers.salary) AS Price_of_Project,
+	projects._descryption AS Project
+FROM developer_customers_projects
+LEFT JOIN developers
+ON developer_customers_projects.developer_ref = developers._id
+LEFT JOIN customer_projects
+ON developer_customers_projects.customer_project_ref = customer_projects._id
+LEFT JOIN projects
+ON customer_projects.project_ref = projects._id
+
+GROUP BY
+	projects._descryption
+ORDER BY
+	Price_of_Project
+LIMIT 1﻿
